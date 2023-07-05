80fc22442d4d37219a0d93d0eca7316cf7cf9b99
MMahdiSheykh <momoaban@gmail.com>
2023-07-05 16:24:34 +0330

    completing save user data from Google in database

--- app/Http/Controllers/Auth/GoogleAuthController.php
+++ app/Http/Controllers/Auth/GoogleAuthController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Laravel\Socialite\Facades\Socialite;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class GoogleAuthController extends Controller
 {
@@ -15,7 +18,26 @@ class GoogleAuthController extends Controller
 
     public function callback()
     {
+        try {
             $googleUser = Socialite::driver('google')->stateless()->user();
-            dd($googleUser->email);
+            $user = User::where('email', $googleUser->email)->first();
+
+            if ($user) {
+                auth()->loginUsingId($user->id);
+            } else {
+                $newUser = User::create([
+                    'name' => $googleUser->name,
+                    'email' => $googleUser->email,
+                    'password' => bcrypt(Str::random())
+                ]);
+                auth()->loginUsingId($newUser->id);
+            }
+            Alert::success('Welcome','Your login was successful!');
+            return redirect('/');
+
+        } catch (\Exception $e) {
+            Alert::error('Login failed!','Your login was not successful!');
+            return redirect('/');
+        }
     }
 }
