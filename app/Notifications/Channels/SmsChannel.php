ec65d03462c12e0a83cddd915b5e770a34d99a97
MMahdiSheykh <momoaban@gmail.com>
2023-07-15 16:33:42 +0330

    enabling queue feature for notifications

--- app/Notifications/Channels/SmsChannel.php
+++ app/Notifications/Channels/SmsChannel.php
@@ -2,10 +2,14 @@
 
 namespace App\Notifications\Channels;
 
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Notification;
 
-class SmsChannel
+class SmsChannel implements ShouldQueue
 {
+    use Queueable;
+
     public function send($notifiable, Notification $notification)
     {
 
--- app/Notifications/EmailLoginNotification.php
+++ app/Notifications/EmailLoginNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class EmailLoginNotification extends Notification
+class EmailLoginNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
