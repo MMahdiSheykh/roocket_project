1d2495442b8445b4b89ce5b5ff1902d6f600d363
MMahdiSheykh <momoaban@gmail.com>
2023-07-15 15:52:42 +0330

    add Google reCAPTCHA for forgot password section

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Rules\Recaptcha;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 
@@ -19,4 +21,12 @@ class ForgotPasswordController extends Controller
     */
 
     use SendsPasswordResetEmails;
+
+    protected function validateEmail(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'g-recaptcha-response' => ['required', new Recaptcha],
+        ]);
+    }
 }
