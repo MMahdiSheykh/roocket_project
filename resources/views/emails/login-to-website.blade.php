ab6a6887a81970a510f87e5e721753d1f6758eab
MMahdiSheykh <momoaban@gmail.com>
2023-07-15 13:56:56 +0330

    customize reset password email message

---
+++ resources/views/emails/login-to-website.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Login to Website</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta iusto impedit quod nemo, excepturi eveniet fugit magni ipsa qui illo, officia velit? Suscipit itaque illum consequatur commodi, vel dignissimos consectetur.</p>
+</body>
+</html>
